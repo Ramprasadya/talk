6733d00530e5b8c161c15f6c752c1f729d183c65
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-04-06 14:48:11 +0530

    google login

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app); // ✅ correct way
+export const provider = new GoogleAuthProvider();
+export const db =getFirestore(app);
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Grid,
+  IconButton,
+  Message,
+  Panel,
+  Row,
+} from "rsuite";
+import GoogleIcon from "@rsuite/icons/Google";
+import "../styles/utility.scss";
+import { getAdditionalUserInfo, signInWithPopup } from "firebase/auth";
+import { auth, db, provider } from "../../firebase";
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const SignInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result;
+      const isNewUser =
+        result._tokenResponse?.isNewUser ??
+        result.additionalUserInfo?.isNewUser;
+      console.log(user);
+      if (isNewUser) {
+        // await setDoc(doc(db, "users", user.uid), {
+        //   name: user.displayName,
+        //   email: user.email,
+        //   photoURL: user.photoURL,
+        //   createdAt: new Date(),
+        //   uid: user.uid,
+        // });
 
-export default SignIn
+        console.log("New user info saved to Firestore.");
+      }
+      //  if(additionalUserInfo.isNewUser){
+      //   Database.ref(`/profiles/${user.uid}`)
+      //  }
+    } catch (error) {
+      <Message type="info">{error.message}</Message>;
+    }
+  };
+  return (
+    <>
+      <Container>
+        <Grid className="mt-page">
+          <Row>
+            <Col xs={24} md={12} mdOffset={6}>
+              <Panel>
+                <div className="text-center">
+                  <h2>Welcome to Chat app</h2>
+                  <p>Progressive chat plateform</p>
+                </div>
+                <div>
+                  <Button block color="green" onClick={SignInWithGoogle}>
+                    <IconButton icon={<GoogleIcon />} /> Continue with google
+                  </Button>
+                </div>
+              </Panel>
+            </Col>
+          </Row>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default SignIn;
